4890ef90d16300c329ec91aa0f33f60b350021cc
xtgeoviz-github-action <xtgeoviz-github-action>
2024-06-28 07:20:00 +0000

    Update Github Pages

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.1.dev2+gbb95a3f',
+    VERSION: '0.1.1.dev3+g00e1931',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
