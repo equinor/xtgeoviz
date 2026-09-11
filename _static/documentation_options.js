eefd26709af466bbf35132284dea497e8fe5131d
xtgeoviz-github-action <xtgeoviz-github-action>
2026-09-11 00:39:25 +0000

    Update Github Pages

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.3.3.dev18+g1546e888d',
+    VERSION: '0.3.3.dev18+g1546e888d.d20260911',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
